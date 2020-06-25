d0d58cb7263ace67728375c38527e54c981d3af9
Ahmad Thoriq Najahi <najahi@chips-projects.xyz>
2020-06-25 01:20:21 +0000

    audio-kernel: Use power efficient workingqueues
    Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

--- techpack/audio/4.0/asoc/codecs/bolero/va-macro.c
+++ techpack/audio/4.0/asoc/codecs/bolero/va-macro.c
@@ -1107,11 +1107,11 @@ static int va_macro_enable_dec(struct snd_soc_dapm_widget *w,
 		 */
 		usleep_range(6000, 6010);
 		/* schedule work queue to Remove Mute */
-		schedule_delayed_work(&va_priv->va_mute_dwork[decimator].dwork,
+		queue_delayed_work(system_power_efficient_wq, &va_priv->va_mute_dwork[decimator].dwork,
 				      msecs_to_jiffies(va_tx_unmute_delay));
 		if (va_priv->va_hpf_work[decimator].hpf_cut_off_freq !=
 							CF_MIN_3DB_150HZ)
-			schedule_delayed_work(
+			queue_delayed_work(system_power_efficient_wq, 
 					&va_priv->va_hpf_work[decimator].dwork,
 					msecs_to_jiffies(hpf_delay));
 		/* apply gain after decimator is enabled */
